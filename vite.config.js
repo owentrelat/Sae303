b6599817897df6baf2918f49fcd38a672f0fcfae
Nrv304 <owentr04@gmail.com>
2025-12-18 11:51:06 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
-import gsap from "@gsap/vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [gsap()],
   base: "/Sae303/",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        },
+      },
     },
   },
 });
